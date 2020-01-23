47624a5a30c46ae01c5c690dd1894f07ff4169b9
Glitch (dsc-radio-bot) <none>
2020-01-23 01:12:57 +0000

    📅🐌 Updated with Glitch

--- commands/leave.js
+++ commands/leave.js
@@ -7,8 +7,9 @@
   async execute(message, args,client) {
     if(!message.member)return;
     const { voiceChannel } = message.member;
+    const {prefix,self_id}= require("../server_setting.json");
     if(message.member.voiceChannelID !== message.guild.me.voiceChannelID)return message.channel.channel.send("jangan ganggu aku!!");
     message.guild.me.voiceChannel.leave();
-    client.user.setActivity("help '. radio'")
+    client.user.setActivity(`help '${prefix}radio'`)
   }
   }
--- server.js
+++ server.js
@@ -37,7 +37,6 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const {prefix, self_id , token, owner} = require(__dirname +'/server_setting.json');
 const {channel_limit} = require(__dirname + "/channel_limit.json");
-const {target} = require(__dirname + "/target.json");
 //settup commands folder
 const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
 
@@ -51,58 +50,13 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-//Member Join
-// client.on('guildMemberAdd', async member => {
-//   console.log(member.guild.id);
-//   if (JSON.stringify(welcome_ch).indexOf(member.guild.id)===-1) return;
-  
-//   const commandName = 'card';
-//   if (!client.commands.has(commandName)) return;
-//   const comid = client.commands.get(commandName);
-//   try {
-//       comid.execute(member, null);
-//      // const channel = member.guild.channels.find(
-//      //    ch => ch.name === "welcome"
-//      //  );
-//      //  channel.send(`hello!`);  
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
+
 
 client.on('message', message => {
-   // if (JSON.stringify(channel_limit).indexOf(message.channel.id)===-1 ) return;
-	// console.log(message.content);
-  // console.log(message.member.voiceChannel);
   if (!message.content.startsWith(prefix) || message.author.bot){
-    // try{console.log(message)}catch(err){console.log(err.message)}
-//     if(Number.isInteger(parseInt(message.content)) && message.content.length > 3 && parseInt(message.content).toString().length >= message.content.length){
-//       const comid = client.commands.get("nuklir");
-//       comid.execute(message,message.content.split(/ +/));
-      
-//     };
-    if(message.content.toLowerCase().includes(" gei ") || message.content.toLowerCase().includes(" gai ") || message.content.toLowerCase().includes(" gay ") || message.content.toLowerCase().includes("gayy") || message.content.toLowerCase().includes("gaii")){
-      console.log("detected")
-      const comid = client.commands.get("ur-gay");
-      comid.execute(message);
-    }
-    // if(message.author.id == "240557240438751233"){
-    //   console.log("mati")
-    //   const comid = client.commands.get("hmpess");
-    //   comid.execute(message);
-    // }
-    if (JSON.stringify(target).indexOf(message.author.id)!==-1 ){
-      console.log("warn_react")
-      const comid = client.commands.get("warn_react");
-      comid.execute(message);
-    };
-    // if(message.author.id == "626004814249197579"){
-    //   console.log("bkp")
-    //   const comid = client.commands.get("bkp");
-    //   comid.execute(message);
-    // }
+    //lakukan sesuatu jika message tampa prefix
   };
-  // console.log("no prefix")
+  
   const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
   console.log(message.channel.id);
@@ -110,8 +64,8 @@ client.on('message', message => {
   const comid = client.commands.get(commandName);
   if( message.guild === null || comid.AnyChannel){
     // lakukan sesuatu ketika DM
-    // return null;
-    console.log(client.users.get(self_id))
+    console.log(client.users.me.id);
+    return null;
   }else{
     // limit untuk channel tertentu
     if (JSON.stringify(channel_limit).indexOf(message.channel.id)===-1 ) return;
@@ -131,5 +85,4 @@ client.on('message', message => {
 
 process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
 client.login(token).then(()=>{
-  // client.channels.get("605061939412074520").send("hore")
 });
